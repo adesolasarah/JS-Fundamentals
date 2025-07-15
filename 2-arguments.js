f8a26d740c22c96a8e57ea181224458156bbc38b
adesolasarah <willamssarahadesola@gmail.com>
2025-07-15 14:52:32 +0100

    Add script that prints messages based on number of arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const argumentCount = process.argv.length - 2;
+
+if (argumentCount === 0) {
+  console.log("No argument");
+} else if (argumentCount === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
