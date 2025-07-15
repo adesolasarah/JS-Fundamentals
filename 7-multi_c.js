c0115cc7152a0386bd26d9216af9ef6f59c6d03b
adesolasarah <willamssarahadesola@gmail.com>
2025-07-16 00:22:39 +0100

    Fix: suppress output for negative and zero in 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,12 @@
-
 const count = Number(process.argv[2]);
 
-if (Number.isInteger(count) && count > 0) {
-  for (let i = 0; i < count; i++) {
-    console.log("C is fun");
+if (!isNaN(count)) {
+  if (count > 0 && Number.isInteger(count)) {
+    for (let i = 0; i < count; i++) {
+      console.log("C is fun");
+    }
   }
+  // Do nothing if count is negative or 0
 } else {
   console.log("Missing number of occurrences");
 }
