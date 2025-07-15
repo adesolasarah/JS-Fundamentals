3e32b4aa736aedf9439e8a31b1aae16cb2423421
adesolasarah <willamssarahadesola@gmail.com>
2025-07-15 15:08:34 +0100

    Print 'C is fun' x times or show error if invalid

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const count = parseInt(process.argv[2]);
+
+if (!isNaN(count) && count > 0) {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
